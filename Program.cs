70453d6a1496df6ebaae32d7d44dfce3cc725a94
Minh Tuyết <lyminhtuyet1110@gmail.com>
2026-03-11 23:23:20 +0700

    Test adminweb

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+﻿using FurnitureStoreData.Context;
+using Microsoft.EntityFrameworkCore;
+
 namespace FurnitureStoreWeb
 {
     public class Program
@@ -6,8 +9,19 @@ namespace FurnitureStoreWeb
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            // Đăng ký DbContext (Bạn đã làm đúng)
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            // 1. CẤU HÌNH SESSION (Dùng cho Giỏ hàng và Đăng nhập)
+            builder.Services.AddSession(options => {
+                options.IdleTimeout = TimeSpan.FromMinutes(30); // Thời gian sống của Session
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
 
             var app = builder.Build();
 
@@ -15,16 +29,24 @@ namespace FurnitureStoreWeb
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseRouting();
 
+            // 2. KÍCH HOẠT SESSION (Bắt buộc phải nằm giữa UseRouting và UseAuthorization)
+            app.UseSession();
+
             app.UseAuthorization();
 
             app.MapStaticAssets();
+
+            // 3. MAP ROUTE CHO AREA ADMIN (Phải đặt trước route default)
+            app.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}")
